068e041b2991353a89a2ca20e310b503b9a19e86
RageKit <bgattet@gmail.com>
2016-11-17 17:32:06 -0500

    Corrected useless dictionary

--- Assets/WaveFunctionCollapse.cs
+++ Assets/WaveFunctionCollapse.cs
@@ -2,6 +2,23 @@
 using System.Collections;
 using System.Collections.Generic;
 
+
+
+public class Wave
+{
+	public Module2DArray[] patterns;
+	public bool[] coefficient;
+
+	public Wave (Module2DArray[] basePatterns)
+	{
+		patterns = basePatterns;
+		coefficient = new bool[patterns.Length];
+		for (int i = 0; i < coefficient.Length; i++) {
+			coefficient [i] = true;
+		}
+	}
+}
+
 public class WaveFunctionCollapse : MonoBehaviour
 {
 	public Grid baseGrid;
@@ -10,47 +27,51 @@ public class WaveFunctionCollapse : MonoBehaviour
 	public int n;
 
 
-	public Dictionary<Vector2,Module2DArray> patterns;
+	public Module2DArray[] patterns;
 
 	public void Build ()
 	{
 		int gridW = (int)baseGrid.gridSize.x;
 		int gridH = (int)baseGrid.gridSize.y;
 
-		patterns = new Dictionary<Vector2, Module2DArray> ();
+		patterns = new Module2DArray[(gridW - n + 1) * (gridH - n + 1)];
 		Debug.Log (baseGrid.content);
 
-		for (int i = 0; i < (gridW - n + 1); i++) {
-			for (int j = 0; j < gridH - n + 1; j++) {
-				for (int k = 0; k < n; k++) {
-
-					for (int l = 0; l < n; l++) {
-
-
-						Module2DArray lol = baseGrid.content.cut (i, j, i + (n - 1), j + (n - 1));
-
-						patterns [new Vector2 (i, j)] = lol;
-
-
-					}
-
-				}
+		for (int i = 0, l = 0; i < (gridW - n + 1); i++) {
+			for (int j = 0; j < gridH - n + 1; j++,l++) {
+				patterns [l] = baseGrid.content.cut (i, j, i + (n - 1), j + (n - 1));
 			}
 
 		}
 
-		foreach (KeyValuePair<Vector2, Module2DArray> entry in patterns) {
-			string ret = "";
-			for (int i = 0; i < entry.Value.Length; i++) {
-				if (entry.Value [i].content == null) {
-					ret += "0";
-				} else {
-					ret += "1";
-				}
-			}
+		int outW = (int)targetGrid.gridSize.x;
+		int outH = (int)targetGrid.gridSize.y;
 
-			Debug.Log (ret);
+		Wave[] ws = new Wave[outW * outH];
+
+		for (int i = 0; i < outW * outH; i++) {
+			ws [i] = new Wave (patterns);
 		}
+
+
+		//STARTING THE ALGO;
+
+//		for (int i = 0; i < patterns.Length; i++) {
+//			
+//		
+//			string ret = "";
+//			for (int j = 0; j < patterns [i].Length; j++) {
+//				if (patterns [i] [j].content == null) {
+//					ret += "0";
+//				} else {
+//					ret += "1";
+//				}
+//			}
+//
+//			Debug.Log (ret);
+//		}
+//
+
 	}
 
 
